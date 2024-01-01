fda518344c92337d55a37cd9d24b92f34740c5ce
SeongTaek <zzoladuck1453@gmail.com>
2024-01-01 23:30:12 +0900

    Update BFS1.cpp

--- BFS/BFS1.cpp
+++ BFS/BFS1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <algorithm>
+#include <queue>
 using namespace std;
+#define X first
+#define Y second
+int board[500][500];
+bool vis[500][500];
 
 int main(){
-  cout << "Hello";
+  
 }
